cc41f773d22d0174619ec4c5676533a18b252acb
Lateef Adeniji-Adele <adenijiadele@yahoo.com>
2018-01-11 20:25:28 +0000

    YGB-15 Implement creating a company with uuid and name

--- src/main/java/com/yaegar/books/model/Company.java
+++ src/main/java/com/yaegar/books/model/Company.java
@@ -1,18 +1,29 @@
 package com.yaegar.books.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+@DynamoDBTable(tableName = "Company")
 public class Company {
     private String name;
     private String uuid;
 
-    public void setName(final String name) {
-        this.name = name;
+    @DynamoDBHashKey
+    public String getUuid() {
+        return uuid;
     }
 
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }
 
-    public String getUuid() {
-        return uuid;
+    @DynamoDBRangeKey
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
     }
 }
